dc98245d6f7c7d3ccc433ff80ec82cd9575bc06a
Alexandr-Mayyura <sonickill@yandex.ru>
2022-09-16 01:21:27 +0300

    setup views

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,16 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var redColorView: UIView!
+    @IBOutlet var redColorView: UIView!
     
-    @IBOutlet weak var yellowColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
     
-    @IBOutlet weak var greenColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    
+    
+    @IBOutlet var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nextButton.layer.cornerRadius = 12
+        
+        redColorView.backgroundColor = .red
+        redColorView.layer.cornerRadius = redColorView.frame.width / 2 - 10
+        redColorView.alpha = 0.3
+        
+        yellowColorView.backgroundColor = .yellow
+        yellowColorView.layer.cornerRadius = redColorView.frame.width / 2 - 10
+        yellowColorView.alpha = 0.3
+        
+        greenColorView.backgroundColor = .green
+        greenColorView.layer.cornerRadius = redColorView.frame.width / 2 - 10
+        greenColorView.alpha = 0.3
+        
     }
 
-
+    @IBAction func nextButtonTapped() {
+    }
+    
 }
 
