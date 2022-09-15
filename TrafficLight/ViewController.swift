fd7140146f8f86e7c179bccfc0ac2a8f2960e34c
Alexandr-Mayyura <sonickill@yandex.ru>
2022-09-16 01:06:00 +0300

    Add view outlet

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redColorView: UIView!
+    
+    @IBOutlet weak var yellowColorView: UIView!
+    
+    @IBOutlet weak var greenColorView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
