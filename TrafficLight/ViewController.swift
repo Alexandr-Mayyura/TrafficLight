0bd1f01fed4425a962ca093bd2ab69678566ef4e
Alexandr-Mayyura <sonickill@yandex.ru>
2022-09-16 20:18:41 +0300

    add viewWillLayoutSubviews

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,34 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet var redColorView: UIView!
-    
     @IBOutlet var yellowColorView: UIView!
-    
     @IBOutlet var greenColorView: UIView!
-    
     
     @IBOutlet var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nextButton.layer.cornerRadius = 12
+        
         
         redColorView.backgroundColor = .red
-        redColorView.layer.cornerRadius = redColorView.frame.width / 2 - 10
+        
         redColorView.alpha = 0.3
         
         yellowColorView.backgroundColor = .yellow
-        yellowColorView.layer.cornerRadius = redColorView.frame.width / 2 - 10
         yellowColorView.alpha = 0.3
         
         greenColorView.backgroundColor = .green
-        greenColorView.layer.cornerRadius = redColorView.frame.width / 2 - 10
         greenColorView.alpha = 0.3
         
     }
 
     @IBAction func nextButtonTapped() {
+        
+        nextButton.setTitle("NEXT", for: .normal)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        redColorView.layer.cornerRadius = redColorView.frame.size.width / 2
+        yellowColorView.layer.cornerRadius = redColorView.frame.size.width / 2
+        greenColorView.layer.cornerRadius = redColorView.frame.size.width / 2
+        nextButton.layer.cornerRadius = 12
     }
     
 }
